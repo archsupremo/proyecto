a2878fa6bce5103cac51e705ff43cb80350e4124
Guillermo <jdkdejava@gmail.com>
2016-07-22 00:02:24 +0200

    Cambios para la nube

--- application/controllers/frontend/Portada.php
+++ application/controllers/frontend/Portada.php
@@ -13,9 +13,7 @@ class Portada extends CI_Controller {
       $latitud = 40.4168;
       $longitud = -3.7038;
       $order = '';
-
-      echo "llega"; die();
-
+      
       if($this->input->get('nombre') !== NULL || $this->input->get('tags') !== NULL) {
           if($this->input->get('order_distancia') !== NULL) {
               $distancia = trim($this->input->get('order_distancia'));
